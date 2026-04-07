9aa35192197f46cb79e896dfab4be4a6b4bf31ad
MOkeke-create <Michaelokeke123@gmail.com>
2026-04-07 15:45:34 -0500

    Printed second message in main function

--- src/main/java/com/pluralsight/ProjectUpload.java
+++ src/main/java/com/pluralsight/ProjectUpload.java
@@ -3,5 +3,6 @@ package com.pluralsight;
 public class ProjectUpload {
     public static void main(String[] args) {
         System.out.println("This is my new upload!");
+        System.out.println("I am getting the hang of this!");
     }
 }
